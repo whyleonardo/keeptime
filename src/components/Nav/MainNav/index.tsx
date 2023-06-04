3d827147e359846bf8ff0de7f916085bb8fc536f
whyleonardo <pleasefloyd73@gmail.com>
2023-06-03 23:57:28 -0300

    feat(MainNav): remove prefetch from nav links

--- src/components/Nav/MainNav/index.tsx
+++ src/components/Nav/MainNav/index.tsx
@@ -14,7 +14,7 @@ interface MainNavProps {
 export const MainNav = ({ items }: MainNavProps) => {
 	return (
 		<div className="flex gap-6 md:gap-10">
-			<Link href="/" className="flex items-center space-x-2">
+			<Link href="/" prefetch={false} className="flex items-center space-x-2">
 				<Icons.logo className="h-6 w-6" />
 				<span className="inline-block font-bold">{siteConfig.name}</span>
 			</Link>
@@ -26,6 +26,7 @@ export const MainNav = ({ items }: MainNavProps) => {
 								<Link
 									key={index}
 									href={item.href}
+									prefetch={false}
 									className={cn(
 										'flex items-center text-sm font-medium text-muted-foreground',
 										item.disabled && 'cursor-not-allowed opacity-80'
