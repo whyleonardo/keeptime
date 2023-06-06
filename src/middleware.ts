b7b938bdf7ad89dbce7e0ede2523054918738761
whyleonardo <pleasefloyd73@gmail.com>
2023-06-05 22:49:55 -0300

    feat: create auth config in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,41 +6,36 @@ import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
 
 export async function middleware(req: NextRequest) {
 	const res = NextResponse.next()
+
+	const { pathname } = req.nextUrl
+
 	const supabase = createMiddlewareClient<Database>({ req, res })
-	await supabase.auth.getSession()
-	return res
+
+	const {
+		data: { session }
+	} = await supabase.auth.getSession()
+
+	if (
+		(session && pathname === '/') ||
+		(session && pathname === '/login') ||
+		(session && pathname === '/register')
+	) {
+		const redirectUrl = req.nextUrl.clone()
+		redirectUrl.pathname = '/dashboard'
+
+		return NextResponse.redirect(redirectUrl)
+	}
+
+	if (!session && pathname.includes('/dashboard')) {
+		const redirectUrl = req.nextUrl.clone()
+		redirectUrl.pathname = '/'
+
+		return NextResponse.redirect(redirectUrl)
+	}
+
+	return NextResponse.next()
 }
 
-// import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs"
-
-// import { NextResponse } from "next/server"
-
-// import type { NextRequest } from "next/server"
-
-// export async function middleware(req: NextRequest) {
-// 	const res = NextResponse.next()
-
-// 	const { pathname } = req.nextUrl
-
-// 	const supabase = createMiddlewareClient({ req, res })
-
-// 	const { data: { session } } = await supabase.auth.getSession()
-
-// 	if (!session && pathname === '/dashboard') {
-// 		const redirectUrl = req.nextUrl.clone()
-// 		redirectUrl.pathname = '/'
-
-// 		return NextResponse.redirect(redirectUrl)
-// 	} else if (session && pathname === '/') {
-// 		const redirectUrl = req.nextUrl.clone()
-// 		redirectUrl.pathname = '/dashboard'
-
-// 		return NextResponse.redirect(redirectUrl)
-// 	}
-
-// 	return NextResponse.next()
-// }
-
-// export const config = {
-// 	matchers: ['/dashboard', '/']
-// }
+export const config = {
+	matchers: ['/', '/login', '/register', '/dashboard']
+}
