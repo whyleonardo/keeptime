c1b78f5aa5a2a140808456fcb6a61560629126a5
whyleonardo <pleasefloyd73@gmail.com>
2023-06-06 17:25:53 -0300

    refactor: remove title from dashboard nested pages

--- src/app/dashboard/billing/page.tsx
+++ src/app/dashboard/billing/page.tsx
@@ -1,7 +1,3 @@
 export default function BillingPage() {
-	return (
-		<div className="flex flex-col gap-4">
-			<h1 className="text-2xl font-semibold">Billing</h1>
-		</div>
-	)
+	return <div className="flex flex-col gap-4"></div>
 }
--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -1,7 +1,3 @@
 export default function SettingsPage() {
-	return (
-		<div className="flex flex-col gap-4">
-			<h1 className="text-2xl font-semibold">Settings</h1>
-		</div>
-	)
+	return <div className="flex flex-col gap-4"></div>
 }
