55048d742e035aed14cac74a380236f15f4e1909
whyleonardo <pleasefloyd73@gmail.com>
2023-06-06 17:27:58 -0300

    feat: import sidebar nav

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,8 +1,5 @@
-import Link from 'next/link'
-
-import { buttonVariants } from '@/components/ui/button'
-
-import { siteConfig } from '@/config/site'
+import { DashboardPageTitle } from '@/components/DashboardPageTitle'
+import { SidebarNav } from '@/components/Nav/SidebarNav'
 
 export default function DashboardLayout({
 	children
@@ -11,23 +8,14 @@ export default function DashboardLayout({
 }) {
 	return (
 		<div className="flex min-h-screen gap-10">
-			<aside className="flex w-60 flex-col gap-4 border-r p-6">
-				{siteConfig.sidebarDashboard.map((item) => (
-					<Link
-						prefetch={false}
-						key={item.href}
-						href={item.href}
-						className={buttonVariants({
-							variant: 'ghost',
-							class: 'flex gap-2 items-center'
-						})}
-					>
-						<item.icon className="h-4 w-4" />
-						{item.title}
-					</Link>
-				))}
+			<aside className="hidden w-60 flex-col gap-4 border-r p-6 sm:flex">
+				<SidebarNav />
 			</aside>
-			{children}
+
+			<div className="p-6">
+				<DashboardPageTitle />
+				{children}
+			</div>
 		</div>
 	)
 }
