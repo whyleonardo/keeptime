aecb4f56c215c49827ae96d1381e8bec15c34159
whyleonardo <pleasefloyd73@gmail.com>
2023-06-05 22:49:30 -0300

    feat: fetch types from supabase

--- src/types/supabase.ts
+++ src/types/supabase.ts
@@ -9,7 +9,61 @@ export type Json =
 export interface Database {
 	public: {
 		Tables: {
-			[_ in never]: never
+			memories: {
+				Row: {
+					created_at: string
+					description: string | null
+					id: string
+					is_public: boolean
+					title: string
+					user_id: string
+				}
+				Insert: {
+					created_at?: string
+					description?: string | null
+					id?: string
+					is_public?: boolean
+					title: string
+					user_id: string
+				}
+				Update: {
+					created_at?: string
+					description?: string | null
+					id?: string
+					is_public?: boolean
+					title?: string
+					user_id?: string
+				}
+			}
+			profiles: {
+				Row: {
+					avatar_url: string | null
+					email: string | null
+					full_name: string | null
+					id: string
+					updated_at: string | null
+					username: string | null
+					website: string | null
+				}
+				Insert: {
+					avatar_url?: string | null
+					email?: string | null
+					full_name?: string | null
+					id: string
+					updated_at?: string | null
+					username?: string | null
+					website?: string | null
+				}
+				Update: {
+					avatar_url?: string | null
+					email?: string | null
+					full_name?: string | null
+					id?: string
+					updated_at?: string | null
+					username?: string | null
+					website?: string | null
+				}
+			}
 		}
 		Views: {
 			[_ in never]: never
