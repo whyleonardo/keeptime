2041be308f010babb8c34f97fb7a9412123d2cd2
whyleonardo <pleasefloyd73@gmail.com>
2023-06-07 21:22:21 -0300

    refactor(UserAuthForm): remove redundant code

--- src/components/Forms/UserAuthForm/index.tsx
+++ src/components/Forms/UserAuthForm/index.tsx
@@ -1,124 +1,46 @@
 'use client'
 
-import { useSearchParams } from 'next/navigation'
 import * as React from 'react'
-import { useForm } from 'react-hook-form'
 
 import { Icons } from '@/components/Icons'
 import { buttonVariants } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
 import { toast } from '@/components/ui/use-toast'
 
 import { cn } from '@/lib/utils'
-import { userAuthSchema } from '@/lib/validations/auth'
 import { sbClient as supabase } from '@/services/supabase/client'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { z } from 'zod'
+import { getURL } from '@/utils/getURL'
 
-type UserAuthFormProps = React.HTMLAttributes<HTMLDivElement>
-
-type FormData = z.infer<typeof userAuthSchema>
-
-export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
-	const {
-		register,
-		handleSubmit,
-		formState: { errors }
-	} = useForm<FormData>({
-		resolver: zodResolver(userAuthSchema)
-	})
-	const [isLoading, setIsLoading] = React.useState<boolean>(false)
+export const UserAuthForm = () => {
 	const [isGitHubLoading, setIsGitHubLoading] = React.useState<boolean>(false)
-	const searchParams = useSearchParams()
 
-	async function onSubmit(data: FormData) {
-		// setIsLoading(true)
-
-		// const signInResult = await supabase.auth.sign
-
-		// const signInResult = await signIn('email', {
-		// 	email: data.email.toLowerCase(),
-		// 	redirect: false,
-		// 	callbackUrl: searchParams?.get('from') || '/dashboard'
-		// })
-
-		// setIsLoading(false)
-
-		// if (!signInResult?.ok) {
-		// 	return toast({
-		// 		title: 'Something went wrong.',
-		// 		description: 'Your sign in request failed. Please try again.',
-		// 		variant: 'destructive'
-		// 	})
-		// }
+	async function githubLogin() {
+		setIsGitHubLoading(true)
+		await supabase.auth.signInWithOAuth({
+			provider: 'github',
+			options: {
+				redirectTo: getURL()
+			}
+		})
 
 		return toast({
-			title: 'Check your email',
-			description: 'We sent you a login link. Be sure to check your spam too.'
+			title: 'Sucess',
+			description: 'You have successfully logged in!'
 		})
 	}
 
 	return (
-		<div className={cn('grid gap-6', className)} {...props}>
-			<form onSubmit={handleSubmit(onSubmit)}>
-				<div className="grid gap-2">
-					<div className="grid gap-1">
-						<Label className="sr-only" htmlFor="email">
-							Email
-						</Label>
-						<Input
-							id="email"
-							placeholder="name@example.com"
-							type="email"
-							autoCapitalize="none"
-							autoComplete="email"
-							autoCorrect="off"
-							disabled={isLoading || isGitHubLoading}
-							{...register('email')}
-						/>
-						{errors?.email && (
-							<p className="px-1 text-xs text-red-600">
-								{errors.email.message}
-							</p>
-						)}
-					</div>
-					<button className={cn(buttonVariants())} disabled={isLoading}>
-						{isLoading && (
-							<Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-						)}
-						Sign In with Email
-					</button>
-				</div>
-			</form>
-			<div className="relative">
-				<div className="absolute inset-0 flex items-center">
-					<span className="w-full border-t" />
-				</div>
-				<div className="relative flex justify-center text-xs uppercase">
-					<span className="bg-background text-muted-foreground px-2">
-						Or continue with
-					</span>
-				</div>
-			</div>
-			<button
-				type="button"
-				className={cn(buttonVariants({ variant: 'outline' }))}
-				onClick={() => {
-					setIsGitHubLoading(true)
-					supabase.auth.signInWithOAuth({
-						provider: 'github'
-					})
-				}}
-				disabled={isLoading || isGitHubLoading}
-			>
-				{isGitHubLoading ? (
-					<Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-				) : (
-					<Icons.gitHub className="mr-2 h-4 w-4" />
-				)}{' '}
-				Github
-			</button>
-		</div>
+		<button
+			type="button"
+			className={cn(buttonVariants({ variant: 'outline' }))}
+			onClick={githubLogin}
+			disabled={isGitHubLoading}
+		>
+			{isGitHubLoading ? (
+				<Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+			) : (
+				<Icons.gitHub className="mr-2 h-4 w-4" />
+			)}{' '}
+			Github
+		</button>
 	)
 }
