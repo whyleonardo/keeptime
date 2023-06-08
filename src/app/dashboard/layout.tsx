44c414fba3d6d8aef019549046e7f7e8727c5b19
whyleonardo <pleasefloyd73@gmail.com>
2023-06-07 21:25:43 -0300

    feat: adjust dashboard style layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -7,12 +7,12 @@ export default function DashboardLayout({
 	children: React.ReactNode
 }) {
 	return (
-		<div className="flex min-h-screen gap-10">
-			<aside className="hidden w-60 flex-col gap-4 border-r p-6 sm:flex">
+		<div className="flex min-h-screen w-full">
+			<aside className="hidden w-72 flex-col gap-4 border-r p-4 sm:flex">
 				<SidebarNav />
 			</aside>
 
-			<div className="p-6">
+			<div className="w-full p-6">
 				<DashboardPageTitle />
 				{children}
 			</div>
