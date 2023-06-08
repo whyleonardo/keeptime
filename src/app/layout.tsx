7dcecba798a1b0c94416e4f963ff751aaf2a3583
whyleonardo <pleasefloyd73@gmail.com>
2023-06-08 01:02:57 -0300

    feat: add full height on html tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,22 +35,22 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
 	return (
 		<>
-			<html lang="en" suppressHydrationWarning>
+			<html className="h-full" lang="en" suppressHydrationWarning>
 				{/* eslint-disable-next-line @next/next/no-head-element*/}
 				<head />
 				<body
 					className={cn(
-						'bg-background font-sans antialiased min-h-[calc(100vh-4rem)] overflow-y-hidden overflow-x-hidden',
+						'bg-background font-sans antialiased h-full min-h-[calc(100vh-4rem)] overflow-y-hidden overflow-x-hidden',
 						fontSans.variable
 					)}
 				>
 					<AuthProvider>
 						<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-							<div className="relative flex flex-col ">
+							<div className="relative flex h-full flex-col">
 								{/* eslint-disable-next-line @typescript-eslint/ban-ts-comment*/}
 								{/* @ts-ignore -- Server Component */}
 								<PageHeader />
-								<div className="flex-1">{children}</div>
+								<div className="h-full flex-1">{children}</div>
 							</div>
 							<ScreenIndicator />
 							<Toaster />
