f8112d9f32f80348b11cf81062a03a11c5019a68
whyleonardo <pleasefloyd73@gmail.com>
2023-06-07 21:22:54 -0300

    feat(MainNav): add color to logo icon

--- src/components/Nav/MainNav/index.tsx
+++ src/components/Nav/MainNav/index.tsx
@@ -9,7 +9,7 @@ export const MainNav = () => {
 	return (
 		<div className="flex gap-6 md:gap-10">
 			<Link href="/" prefetch={false} className="flex items-center space-x-2">
-				<Icons.logo className="h-6 w-6" />
+				<Icons.logo className="fill-foreground h-6 w-6" />
 				<span className="inline-block font-bold">{siteConfig.name}</span>
 			</Link>
 		</div>
