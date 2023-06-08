f623133fc7ce3c3dca31fd7b1dddf180385dd0c8
whyleonardo <pleasefloyd73@gmail.com>
2023-06-07 21:16:33 -0300

    feat: fetch database schema from supabase

---
+++ src/types/profile.ts
@@ -0,0 +1,13 @@
+export type Profile = {
+	profile:
+		| {
+				avatar_url: string | null
+				email: string | null
+				full_name: string | null
+				id: string
+				updated_at: string | null
+				username: string | null
+				website: string | null
+		  }
+		| undefined
+}
