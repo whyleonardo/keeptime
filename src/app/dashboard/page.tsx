c4473fb4bea10937ba87cc14711021ea4ca30391
whyleonardo <pleasefloyd73@gmail.com>
2023-06-08 01:04:12 -0300

    feat(DashboardPage): abstract memory card to component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,15 @@
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Metadata } from 'next'
+
+import { MemoryCard } from '@/components/Cards/MemoryCard'
 
 import { sbServer as supabase } from '@/services/supabase/server'
 
 export const revalidate = 60
+
+export const metadata: Metadata = {
+	title: 'Dashboard',
+	description: 'Manage your memories.'
+}
 
 export default async function DashboardPage() {
 	const memories = (await supabase.from('memories').select('*')).data
@@ -14,25 +21,14 @@ export default async function DashboardPage() {
 		profiles.find((profile) => profile.id === memory.user_id && profile)
 
 	return (
-		<div className="flex gap-2">
+		<div className="mt-6 flex flex-wrap justify-center gap-2 rounded-md border p-6">
 			{memories &&
 				memories.map((memory) => (
-					<div key={memory.id}>
-						<h2>{memory.title}</h2>
-						<div className="flex items-center gap-1 border">
-							<p>{targetProfile(memory)?.full_name}</p>
-
-							<Avatar className="h-6 w-6">
-								{/* eslint-disable-next-line @typescript-eslint/ban-ts-comment*/}
-								{/* @ts-ignore */}
-								<AvatarImage src={targetProfile(memory)?.avatar_url} />
-								<AvatarFallback>
-									{targetProfile(memory)?.full_name?.at(0)?.toUpperCase()}
-								</AvatarFallback>
-							</Avatar>
-						</div>
-						<p>{memory.description}</p>
-					</div>
+					<MemoryCard
+						key={memory.id}
+						memory={memory}
+						targetProfile={targetProfile}
+					/>
 				))}
 		</div>
 	)
