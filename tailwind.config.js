f98459f63f157825cd93fe22474766fb6f8c4923
whyleonardo <pleasefloyd73@gmail.com>
2023-06-07 21:15:46 -0300

    feat(tailwind-config): add success variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const { fontFamily } = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	darkMode: ['class'],
-	content: ['src/**/*.{ts,tsx}', 'components/**/*.{ts,tsx}'],
+	content: ['src/**/*.{ts,tsx}'],
 	theme: {
 		container: {
 			center: true,
@@ -31,6 +31,10 @@ module.exports = {
 				destructive: {
 					DEFAULT: 'hsl(var(--destructive))',
 					foreground: 'hsl(var(--destructive-foreground))'
+				},
+				success: {
+					DEFAULT: 'hsl(var(--success))',
+					foreground: 'hsl(var(--success-foreground))'
 				},
 				muted: {
 					DEFAULT: 'hsl(var(--muted))',
