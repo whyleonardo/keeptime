6fb2e90af7fa015a9af3ffe9f84ad03bd8572500
whyleonardo <pleasefloyd73@gmail.com>
2023-06-07 21:25:04 -0300

    refactor(LoginPage): remove redundant code

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -30,22 +30,12 @@ export default function LoginPage() {
 			<div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
 				<div className="flex flex-col space-y-2 text-center">
 					<Icons.logo className="mx-auto h-6 w-6" />
-					<h1 className="text-2xl font-semibold tracking-tight">
-						Welcome back
-					</h1>
+					<h1 className="text-2xl font-semibold tracking-tight">Welcome</h1>
 					<p className="text-muted-foreground text-sm">
 						Enter your email to sign in to your account
 					</p>
 				</div>
 				<UserAuthForm />
-				<p className="text-muted-foreground px-8 text-center text-sm">
-					<Link
-						href="/register"
-						className="hover:text-brand underline underline-offset-4"
-					>
-						Don&apos;t have an account? Sign Up
-					</Link>
-				</p>
 			</div>
 		</div>
 	)
