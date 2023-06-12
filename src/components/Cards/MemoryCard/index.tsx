b1f06e20fc6ac5179005f616c23a7ea10c45380c
whyleonardo <pleasefloyd73@gmail.com>
2023-06-12 10:38:17 -0300

    feat(MemoryCard): change card style

--- src/components/Cards/MemoryCard/index.tsx
+++ src/components/Cards/MemoryCard/index.tsx
@@ -1,4 +1,8 @@
+import Link from 'next/link'
+
+import { Icons } from '@/components/Icons'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
 import {
 	Card,
 	CardContent,
@@ -18,36 +22,41 @@ interface MemoryCardProps {
 
 export const MemoryCard = ({ memory, targetProfile }: MemoryCardProps) => {
 	return (
-		<Card className="flex h-full w-full flex-col items-center overflow-hidden lg:w-4/6">
+		<Card className="flex h-full w-full flex-col items-center overflow-hidden lg:w-5/6">
 			<CardHeader>
-				<CardTitle className="text-center">{memory.title}</CardTitle>
+				<CardTitle>{memory.title}</CardTitle>
+				{/*  */}
 			</CardHeader>
 
 			<CardContent className="flex w-full flex-col items-center lg:w-3/4">
 				<div className="flex w-full flex-col items-center">
-					<div className="mb-2 flex w-full justify-between px-4">
-						<div className="flex items-center gap-1">
-							<Avatar className="h-5 w-5">
-								{/* eslint-disable-next-line @typescript-eslint/ban-ts-comment*/}
-								{/* @ts-ignore */}
-								<AvatarImage src={targetProfile(memory)?.avatar_url} />
-								<AvatarFallback>
-									{targetProfile(memory)?.full_name?.at(0)?.toUpperCase()}
-								</AvatarFallback>
-							</Avatar>
+					<span className="text-muted-foreground mb-2 self-center">
+						{dateFormat(new Date(memory.created_at))}
+					</span>
 
-							<span className="text-muted-foreground">
-								{targetProfile(memory)?.username ||
-									targetProfile(memory)?.full_name}
-							</span>
-						</div>
-
-						<span className="text-muted-foreground">
-							{dateFormat(new Date(memory.created_at))}
-						</span>
+					<div className="relative h-[28rem] w-full rounded-md bg-gradient-to-br from-purple-500 via-red-500 to-orange-500">
+						<button className="absolute right-4 top-4 transition-transform hover:scale-125">
+							<Icons.more />
+						</button>
 					</div>
+					<Link
+						href={`/dashboard/profile/${targetProfile(memory)?.username}`}
+						className="group ml-2 mt-2 flex items-center gap-1 self-start "
+					>
+						<Avatar className="h-5 w-5">
+							{/* eslint-disable-next-line @typescript-eslint/ban-ts-comment*/}
+							{/* @ts-ignore */}
+							<AvatarImage src={targetProfile(memory)?.avatar_url} />
+							<AvatarFallback>
+								{targetProfile(memory)?.full_name?.at(0)?.toUpperCase()}
+							</AvatarFallback>
+						</Avatar>
 
-					<div className="h-[28rem] w-[28rem] max-w-[100%] rounded-md border border-dashed"></div>
+						<span className="text-muted-foreground group-hover:text-muted-foreground/80 font-semibold transition-colors">
+							{targetProfile(memory)?.username ||
+								targetProfile(memory)?.full_name}
+						</span>
+					</Link>
 				</div>
 
 				<CardDescription className="m-2 max-w-full self-start whitespace-pre-wrap">
