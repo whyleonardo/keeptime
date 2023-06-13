58b0764ed3557a4e9d83f9bf8977932ae8cdb152
whyleonardo <pleasefloyd73@gmail.com>
2023-06-13 12:17:25 -0300

    feat: create memory form

---
+++ src/components/Forms/CreateMemoryForm/index.tsx
@@ -0,0 +1,155 @@
+'use client'
+
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+
+import { Icons } from '@/components/Icons'
+import { Button } from '@/components/ui/button'
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { MediaPicker } from '@/components/ui/media-picker'
+import { ScrollArea } from '@/components/ui/scroll-area'
+import { Switch } from '@/components/ui/switch'
+import { Textarea } from '@/components/ui/textarea'
+
+import { memorySchema } from '@/lib/validations/memory'
+import { sbClient as supabase } from '@/services/supabase/client'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+export const CreateMemoryForm = ({
+	setOpen
+}: {
+	setOpen: React.Dispatch<React.SetStateAction<boolean>>
+}) => {
+	const [loading, setLoading] = useState(false)
+
+	const form = useForm<z.infer<typeof memorySchema>>({
+		resolver: zodResolver(memorySchema),
+		defaultValues: {
+			title: '',
+			description: '',
+			is_public: false
+		}
+	})
+
+	async function onSubmit(values: z.infer<typeof memorySchema>) {
+		setLoading(true)
+		const {
+			data: { user }
+		} = await supabase.auth.getUser()
+
+		console.log(values.media)
+
+		const mediaType = values.media.type.includes('image') ? 'image' : 'video'
+
+		if (!user) return
+
+		const { data } = await supabase.storage
+			.from('medias')
+			.upload(`${mediaType}/${crypto.randomUUID()}`, values.media)
+
+		await supabase
+			.from('memories')
+			.insert({
+				title: values.title,
+				description: values.description,
+				is_public: values.is_public,
+				user_id: user?.id,
+				media_path: data?.path
+			})
+			.throwOnError()
+
+		form.reset()
+
+		setOpen(false)
+		setLoading(false)
+	}
+
+	return (
+		<Form {...form}>
+			<ScrollArea className="h-screen bg-transparent">
+				<form
+					onSubmit={form.handleSubmit(onSubmit)}
+					className="min-h-max space-y-8 overflow-y-auto pb-20"
+				>
+					<FormField
+						control={form.control}
+						name="title"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Title</FormLabel>
+								<FormControl>
+									<Input placeholder="Memory title" {...field} />
+								</FormControl>
+
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+
+					<FormField
+						control={form.control}
+						name="description"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Description</FormLabel>
+								<FormControl>
+									<Textarea
+										placeholder="Give an description to your memory"
+										{...field}
+										className="resize-none"
+									/>
+								</FormControl>
+
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+
+					<FormField
+						control={form.control}
+						name="is_public"
+						render={({ field }) => (
+							<FormItem>
+								<div className="flex items-center gap-2">
+									<FormLabel>Is Public?</FormLabel>
+									<FormControl>
+										<Switch
+											checked={field.value}
+											onCheckedChange={field.onChange}
+										/>
+									</FormControl>
+								</div>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+
+					<FormField
+						control={form.control}
+						name="media"
+						render={({ field }) => (
+							<FormItem>
+								<FormControl>
+									<MediaPicker field={field} />
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<Button type="submit">
+						{loading ? <Icons.spinner className="animate-spin" /> : 'Create'}
+					</Button>
+				</form>
+			</ScrollArea>
+		</Form>
+	)
+}
