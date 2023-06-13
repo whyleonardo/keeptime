9f28cf6119a22ef20f00111d2a1d01ff80eac84b
whyleonardo <pleasefloyd73@gmail.com>
2023-06-13 12:19:10 -0300

    feat: add media type

--- src/lib/validations/memory.ts
+++ src/lib/validations/memory.ts
@@ -3,5 +3,6 @@ import { z } from 'zod'
 export const memorySchema = z.object({
 	title: z.string().min(3).max(20),
 	is_public: z.boolean(),
-	description: z.string().min(3).max(240)
+	description: z.string().min(3).max(240),
+	media: z.any()
 })
