c61efb0286fefcdcbefcc11de954a65f4fce9b10
whyleonardo <pleasefloyd73@gmail.com>
2023-06-15 16:18:14 -0300

    feat: change max string title lenght

--- src/lib/validations/memory.ts
+++ src/lib/validations/memory.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 export const memorySchema = z.object({
-	title: z.string().min(3).max(20),
+	title: z.string().min(3).max(30),
 	is_public: z.boolean(),
 	description: z.string().min(3).max(240),
 	media: z.any()
