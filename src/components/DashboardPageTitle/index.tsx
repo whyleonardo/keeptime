4f1d4fdc900c05968b9edce155c8137979e6308a
whyleonardo <pleasefloyd73@gmail.com>
2023-06-15 16:21:10 -0300

    refactor: add logic to capitalize page title

--- src/components/DashboardPageTitle/index.tsx
+++ src/components/DashboardPageTitle/index.tsx
@@ -2,15 +2,24 @@
 
 import { usePathname } from 'next/navigation'
 
+import { cn } from '@/lib/utils'
+
 export const DashboardPageTitle = () => {
 	const pathname = usePathname()
+
+	const profilePath = pathname.includes('/profile')
 
 	const getLastWordRegexp = /\/([^/]+)$/
 	const match = pathname.match(getLastWordRegexp)
 	const lastWord = match ? match[1] : null
 
 	return (
-		<h1 className="mb-2 text-4xl font-bold capitalize tracking-tight">
+		<h1
+			className={cn(
+				!profilePath && 'capitalize',
+				'mb-2 text-4xl font-bold tracking-tight'
+			)}
+		>
 			{lastWord}
 		</h1>
 	)
