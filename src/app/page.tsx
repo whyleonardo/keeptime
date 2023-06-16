d4196f917d7b7912e6ecd7f544d47c8f0d915814
whyleonardo <pleasefloyd73@gmail.com>
2023-06-16 12:21:52 -0300

    feat: create header in index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,57 @@
 import Link from 'next/link'
 
 import { Icons } from '@/components/Icons'
+import { ThemeToggle } from '@/components/ThemeToggle'
 import { buttonVariants } from '@/components/ui/button'
 
+import { siteConfig } from '@/config/site'
 import { cn } from '@/lib/utils'
 
 export default function IndexPage() {
 	return (
-		<section className="flex min-h-[calc(100vh-5rem)] flex-col items-center justify-center space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-24">
-			<div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
-				<Icons.logo className="fill-primary h-14 w-14 sm:h-20 sm:w-20" />
+		<>
+			<header className="flex h-16 w-full items-center justify-between px-12">
+				<div className="flex items-center space-x-12">
+					<div className="flex items-center space-x-2">
+						<Icons.logo className="h-6 w-6 fill-foreground" />
+						<span className="inline-block font-bold">{siteConfig.name}</span>
+					</div>
 
-				<h1 className="font-heading text-4xl font-black sm:text-5xl md:text-6xl lg:text-7xl">
-					Relive, Remember, Keeptime
-				</h1>
+					<nav className="flex  items-center gap-6 text-sm font-medium text-foreground/60 transition-colors">
+						<Link
+							className="hover:text-muted-foreground/80"
+							href="/"
+							prefetch={false}
+						>
+							Home
+						</Link>
+						<Link className="hover:text-muted-foreground/80" href="/login">
+							Login
+						</Link>
+					</nav>
+				</div>
 
-				<p className="text-muted-foreground max-w-[42rem] leading-normal sm:text-xl sm:leading-8">
-					I&apos;m building a web app with Next.js 13 and open sourcing
-					everything. Follow along as we figure this out together.
-				</p>
+				<ThemeToggle />
+			</header>
 
-				<Link href="/login" className={cn(buttonVariants({ size: 'lg' }))}>
-					Get Started
-				</Link>
-			</div>
-		</section>
+			<section className="flex min-h-[calc(100vh-5rem)] flex-col items-center justify-center space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-24">
+				<div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
+					<Icons.logo className="h-14 w-14 fill-primary sm:h-20 sm:w-20" />
+
+					<h1 className="font-heading text-4xl font-black sm:text-5xl md:text-6xl lg:text-7xl">
+						Relive, Remember, Keeptime
+					</h1>
+
+					<p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+						I&apos;m building a web app with Next.js 13 and open sourcing
+						everything. Follow along as we figure this out together.
+					</p>
+
+					<Link href="/login" className={cn(buttonVariants({ size: 'lg' }))}>
+						Get Started
+					</Link>
+				</div>
+			</section>
+		</>
 	)
 }
