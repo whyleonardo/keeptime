9d8f768bfe060f26afc41a8f5fca22de5b9bd0f0
whyleonardo <pleasefloyd73@gmail.com>
2023-06-16 12:23:34 -0300

    feat: add login with discord

--- src/components/Forms/UserAuthForm/index.tsx
+++ src/components/Forms/UserAuthForm/index.tsx
@@ -12,6 +12,7 @@ import { getURL } from '@/utils/getURL'
 
 export const UserAuthForm = () => {
 	const [isGitHubLoading, setIsGitHubLoading] = React.useState<boolean>(false)
+	const [isDiscordLoading, setIsDiscordLoading] = React.useState<boolean>(false)
 
 	async function githubLogin() {
 		setIsGitHubLoading(true)
@@ -22,6 +23,25 @@ export const UserAuthForm = () => {
 			}
 		})
 
+		setIsGitHubLoading(false)
+
+		return toast({
+			title: 'Sucess',
+			description: 'You have successfully logged in!'
+		})
+	}
+
+	async function discordLogin() {
+		setIsDiscordLoading(true)
+		await supabase.auth.signInWithOAuth({
+			provider: 'discord',
+			options: {
+				redirectTo: getURL()
+			}
+		})
+
+		setIsDiscordLoading(false)
+
 		return toast({
 			title: 'Sucess',
 			description: 'You have successfully logged in!'
@@ -29,18 +49,34 @@ export const UserAuthForm = () => {
 	}
 
 	return (
-		<button
-			type="button"
-			className={cn(buttonVariants({ variant: 'outline' }))}
-			onClick={githubLogin}
-			disabled={isGitHubLoading}
-		>
-			{isGitHubLoading ? (
-				<Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-			) : (
-				<Icons.gitHub className="mr-2 h-4 w-4" />
-			)}{' '}
-			Github
-		</button>
+		<div className="flex flex-col space-y-2">
+			<button
+				type="button"
+				className={cn(buttonVariants({ variant: 'outline' }))}
+				onClick={githubLogin}
+				disabled={isGitHubLoading}
+			>
+				{isGitHubLoading ? (
+					<Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+				) : (
+					<Icons.gitHub className="mr-2 h-4 w-4 fill-primary" />
+				)}{' '}
+				Github
+			</button>
+
+			<button
+				type="button"
+				className={cn(buttonVariants({ variant: 'outline' }))}
+				onClick={discordLogin}
+				disabled={isDiscordLoading}
+			>
+				{isDiscordLoading ? (
+					<Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+				) : (
+					<Icons.discord className="mr-2 h-4 w-4 fill-primary" />
+				)}{' '}
+				Discord
+			</button>
+		</div>
 	)
 }
