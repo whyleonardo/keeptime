791d6ae4d40d1733261538302b1e70ee28bc7cd4
whyleonardo <pleasefloyd73@gmail.com>
2023-06-16 12:23:54 -0300

    feat: add page header in dashboard route

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,5 @@
-import { DashboardPageTitle } from '@/components/DashboardPageTitle'
 import { SidebarNav } from '@/components/Nav/SidebarNav'
+import { PageHeader } from '@/components/PageHeader'
 import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area'
 
 export default function DashboardLayout({
@@ -8,16 +8,22 @@ export default function DashboardLayout({
 	children: React.ReactNode
 }) {
 	return (
-		<div className="flex h-full min-h-screen w-full pb-4">
-			<aside className="hidden w-72 flex-col gap-4 p-4 sm:flex">
-				<SidebarNav />
-			</aside>
+		<>
+			{/* eslint-disable-next-line @typescript-eslint/ban-ts-comment*/}
+			{/* @ts-ignore -- Server Component */}
+			<PageHeader />
 
-			<ScrollArea className="h-full w-full overflow-x-hidden p-6 md:pb-16">
-				<ScrollBar />
-				{/* <DashboardPageTitle /> */}
-				{children}
-			</ScrollArea>
-		</div>
+			<div className="flex h-full min-h-screen w-full pb-4">
+				<aside className="hidden w-72 flex-col gap-4 p-4 sm:flex">
+					<SidebarNav />
+				</aside>
+
+				<ScrollArea className="h-full w-full overflow-x-hidden p-6 md:pb-16">
+					<ScrollBar />
+					{/* <DashboardPageTitle /> */}
+					{children}
+				</ScrollArea>
+			</div>
+		</>
 	)
 }
