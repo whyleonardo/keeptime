bc9d47cee6c09f0f3863d74252dfde5338b9b443
whyleonardo <pleasefloyd73@gmail.com>
2023-06-30 16:29:46 -0300

    feat: add new ui components

---
+++ src/components/ui/badge.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react'
+
+import { cn } from '@/lib/utils'
+import { cva, type VariantProps } from 'class-variance-authority'
+
+const badgeVariants = cva(
+	'focus:ring-ring inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2',
+	{
+		variants: {
+			variant: {
+				default:
+					'bg-primary text-primary-foreground hover:bg-primary/80 border-transparent',
+				secondary:
+					'bg-secondary text-secondary-foreground hover:bg-secondary/80 border-transparent',
+				destructive:
+					'bg-destructive text-destructive-foreground hover:bg-destructive/80 border-transparent',
+				outline: 'text-foreground',
+				pro: 'text-muted-foreground flex items-center gap-1 px-2 py-[0.075rem] text-[0.60rem]'
+			}
+		},
+		defaultVariants: {
+			variant: 'default'
+		}
+	}
+)
+
+export interface BadgeProps
+	extends React.HTMLAttributes<HTMLDivElement>,
+		VariantProps<typeof badgeVariants> {}
+
+function Badge({ className, variant, ...props }: BadgeProps) {
+	return (
+		<div className={cn(badgeVariants({ variant }), className)} {...props} />
+	)
+}
+
+export { Badge, badgeVariants }
