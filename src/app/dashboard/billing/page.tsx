56c1d71ab8e67442936d71f5b3164d6939c6a9ad
whyleonardo <pleasefloyd73@gmail.com>
2023-06-30 16:35:03 -0300

    feat: displays billing form into page

--- src/app/dashboard/billing/page.tsx
+++ src/app/dashboard/billing/page.tsx
@@ -1,10 +1,43 @@
-import { Metadata } from 'next'
+import { redirect } from 'next/navigation'
 
-export const metadata: Metadata = {
-	title: 'Billing',
-	description: 'Manage billing and subscription settings.'
-}
+import { BillingForm } from '@/components/Forms/BillingForm'
 
-export default function BillingPage() {
-	return <div className="flex flex-col gap-4"></div>
+import { stripe } from '@/services/stripe'
+import { getUserSubscriptionPlan } from '@/services/stripe/subscription'
+import { sbServer as supabase } from '@/services/supabase/server'
+
+// import { Metadata } from 'next'
+
+// export const metadata: Metadata = {
+// 	title: 'Billing',
+// 	description: 'Manage billing and subscription settings.'
+// }
+
+export default async function BillingPage() {
+	const user = (await supabase.auth.getUser()).data.user
+
+	if (!user) {
+		redirect('/login')
+	}
+
+	const subscriptionPlan = await getUserSubscriptionPlan(user?.id)
+
+	let isCanceled = false
+	if (subscriptionPlan.isPro && subscriptionPlan.stripe_subscription_id) {
+		const stripePlan = await stripe.subscriptions.retrieve(
+			subscriptionPlan.stripe_subscription_id
+		)
+		isCanceled = stripePlan.cancel_at_period_end
+	}
+
+	return (
+		<div className="flex flex-col gap-4">
+			<BillingForm
+				subscriptionPlan={{
+					...subscriptionPlan,
+					isCanceled
+				}}
+			/>
+		</div>
+	)
 }
