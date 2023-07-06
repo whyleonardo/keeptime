85f2229b4eeff6d6661b9fb2c32554b0d8706c5c
whyleonardo <pleasefloyd73@gmail.com>
2023-07-05 22:43:44 -0300

    feat: add back page button

--- src/components/Nav/MainNav/index.tsx
+++ src/components/Nav/MainNav/index.tsx
@@ -1,15 +1,32 @@
-import * as React from 'react'
+'use client'
+
+import { useRouter, usePathname } from 'next/navigation'
 
 import { Icons } from '@/components/Icons'
+import { Button } from '@/components/ui/button'
 
 import { siteConfig } from '@/config/site'
 
 export const MainNav = () => {
+	const router = useRouter()
+	const pathname = usePathname()
+
+	function onClick() {
+		router.back()
+	}
 	return (
 		<div className="flex gap-6 md:gap-10">
 			<div className="flex select-none items-center space-x-2">
-				<Icons.logo className="fill-foreground h-6 w-6" />
-				<span className="inline-block font-bold">{siteConfig.name}</span>
+				{pathname === '/dashboard' ? (
+					<>
+						<Icons.logo className="fill-foreground h-6 w-6" />
+						<span className="inline-block font-bold">{siteConfig.name}</span>
+					</>
+				) : (
+					<Button onClick={onClick} variant="ghost" size="icon">
+						<Icons.chevronLeft />
+					</Button>
+				)}
 			</div>
 		</div>
 	)
