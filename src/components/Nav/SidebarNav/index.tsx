3b7397cb6aa91bcae487576e8e980faf7a9af61f
whyleonardo <pleasefloyd73@gmail.com>
2023-07-05 22:44:06 -0300

    feat: add config to profile route link

--- src/components/Nav/SidebarNav/index.tsx
+++ src/components/Nav/SidebarNav/index.tsx
@@ -6,8 +6,13 @@ import { usePathname } from 'next/navigation'
 import { buttonVariants } from '@/components/ui/button'
 
 import { siteConfig } from '@/config/site'
+import { clsx } from 'clsx'
 
-export const SidebarNav = () => {
+interface SidebarNavProps {
+	username: string | null | undefined
+}
+
+export const SidebarNav = ({ username }: SidebarNavProps) => {
 	const pathname = usePathname()
 
 	return (
@@ -16,12 +21,20 @@ export const SidebarNav = () => {
 				<Link
 					prefetch={false}
 					key={index}
-					href={item.href}
+					href={
+						item.href.includes('profile')
+							? `${item.href}/${username}`
+							: item.href
+					}
 					className={buttonVariants({
 						variant: 'ghost',
-						className: `flex gap-2 items-center !justify-start ${
-							pathname === item.href && 'bg-primary text-primary-foreground'
-						}`
+						className: clsx(
+							'flex items-center !justify-start gap-2',
+							pathname === item.href && 'bg-primary text-primary-foreground',
+							pathname.includes('/profile') &&
+								item.href.includes('/profile') &&
+								'bg-primary text-primary-foreground'
+						)
 					})}
 				>
 					<item.icon className="h-4 w-4" />
