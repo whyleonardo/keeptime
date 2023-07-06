a97c1fb19b8fa55f7815aed4053ff05bcc0ed48d
whyleonardo <pleasefloyd73@gmail.com>
2023-07-06 11:39:39 -0300

    chore: change colors

--- src/components/ScreenIndicator/index.tsx
+++ src/components/ScreenIndicator/index.tsx
@@ -4,7 +4,7 @@ export const ScreenIndicator = () => {
 	if (process.env.NODE_ENV === 'production') return null
 
 	return (
-		<div className="fixed bottom-1 left-1 z-50 flex items-center justify-center gap-2 rounded-full bg-gray-800 p-3 font-mono text-xs font-bold text-white">
+		<div className="bg-foreground text-background fixed bottom-1 left-1 z-50 flex items-center justify-center gap-2 rounded-full p-3 font-mono text-xs font-bold">
 			<div className="block sm:hidden">xs</div>
 			<div className="hidden sm:block md:hidden">sm</div>
 			<div className="hidden md:block lg:hidden">md</div>
