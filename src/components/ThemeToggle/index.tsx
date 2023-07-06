8bd740933e293e7648c6e0e1c2223b63a2fb9d4b
whyleonardo <pleasefloyd73@gmail.com>
2023-07-05 22:43:13 -0300

    feat: add mobile prop to display the correctly element

--- src/components/ThemeToggle/index.tsx
+++ src/components/ThemeToggle/index.tsx
@@ -6,18 +6,49 @@ import * as React from 'react'
 import { Icons } from '@/components/Icons'
 import { Button } from '@/components/ui/button'
 
-export const ThemeToggle = () => {
+interface ThemeToggleProps {
+	isMobile?: boolean
+}
+
+export const ThemeToggle = ({ isMobile }: ThemeToggleProps) => {
 	const { setTheme, theme } = useTheme()
 
 	return (
-		<Button
-			variant="ghost"
-			size="sm"
-			onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-		>
-			<Icons.sun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-			<Icons.moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-			<span className="sr-only">Toggle theme</span>
-		</Button>
+		<>
+			{isMobile ? (
+				theme === 'light' ? (
+					<>
+						<Icons.sun className="h-4 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+						<button
+							className="flex w-full items-center"
+							onClick={() => setTheme('dark')}
+						>
+							Change Theme
+						</button>
+					</>
+				) : (
+					<>
+						<Icons.moon className="h-4 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+						<button
+							className="flex w-full items-center"
+							onClick={() => setTheme('light')}
+						>
+							Change Theme
+						</button>
+					</>
+				)
+			) : (
+				<Button
+					variant="ghost"
+					size="sm"
+					onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+					className="hidden md:inline-flex"
+				>
+					<Icons.sun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+					<Icons.moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+					<span className="sr-only">Toggle theme</span>
+				</Button>
+			)}
+		</>
 	)
 }
