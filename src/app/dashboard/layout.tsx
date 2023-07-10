61fee8a5b0f5a85cba7e55d1dd8fa500a93b12bb
whyleonardo <pleasefloyd73@gmail.com>
2023-07-10 12:32:10 -0300

    chore: adjust padding bottom in mobile

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -41,7 +41,7 @@ export default async function DashboardLayout({
 
 				<ScrollArea>
 					<ScrollBar />
-					<main className="p-8 ">{children}</main>
+					<main className="px-8 pb-28 pt-8 md:p-8">{children}</main>
 				</ScrollArea>
 
 				<RequireUsernameDialog userId={user.id} />
