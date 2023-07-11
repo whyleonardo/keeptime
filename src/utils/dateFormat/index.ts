2e7c3f15d741bb52d4871976e08aa24cfc619adf
whyleonardo <pleasefloyd73@gmail.com>
2023-07-11 00:02:11 -0300

    feat: add omitDay parameter

--- src/utils/dateFormat/index.ts
+++ src/utils/dateFormat/index.ts
@@ -1,7 +1,7 @@
-export const dateFormat = (date: Date) => {
+export const dateFormat = (date: Date, omitDay = false) => {
 	const formatedDate = new Intl.DateTimeFormat('en-US', {
-		month: 'short',
-		day: 'numeric',
+		month: 'long',
+		day: omitDay ? undefined : 'numeric',
 		year: 'numeric'
 	}).format(date)
 
