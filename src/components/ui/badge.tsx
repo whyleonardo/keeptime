c548ef08483643bac701587dcd7cf23de0a1d0b4
whyleonardo <pleasefloyd73@gmail.com>
2023-07-11 00:08:40 -0300

    style: fix lint

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -4,18 +4,18 @@ import { cn } from '@/lib/utils'
 import { cva, type VariantProps } from 'class-variance-authority'
 
 const badgeVariants = cva(
-	'inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
+	'focus:ring-ring inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2',
 	{
 		variants: {
 			variant: {
 				default:
-					'border-transparent bg-primary text-primary-foreground hover:bg-primary/80',
+					'bg-primary text-primary-foreground hover:bg-primary/80 border-transparent',
 				secondary:
-					'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
+					'bg-secondary text-secondary-foreground hover:bg-secondary/80 border-transparent',
 				destructive:
-					'border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80',
+					'bg-destructive text-destructive-foreground hover:bg-destructive/80 border-transparent',
 				outline: 'text-foreground',
-				pro: 'flex items-center gap-1 px-2 py-[0.075rem] text-[0.60rem] text-muted-foreground'
+				pro: 'text-muted-foreground flex items-center gap-1 px-2 py-[0.075rem] text-[0.60rem]'
 			}
 		},
 		defaultVariants: {
