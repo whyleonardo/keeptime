9465350df2b6492b9ab7819c78f7fab0ab765d2f
whyleonardo <pleasefloyd73@gmail.com>
2023-07-12 17:12:34 -0300

    chore: import get avatar path

--- src/components/Cards/MemoryCard/index.tsx
+++ src/components/Cards/MemoryCard/index.tsx
@@ -20,6 +20,7 @@ import {
 import { sbServer as supabase } from '@/services/supabase/server'
 import { Memory } from '@/types/memory'
 import { dateFormat } from '@/utils/dateFormat'
+import { getAvatarPath } from '@/utils/getAvatarPath'
 import { getMedia } from '@/utils/getMedia'
 import clsx from 'clsx'
 
@@ -48,6 +49,8 @@ export const MemoryCard = async ({
 
 	if (!user) return null
 
+	const userAvatar = getAvatarPath(user.avatar_url)
+
 	return (
 		<Card className="flex h-full w-full flex-col items-center overflow-hidden last:pb-20">
 			<CardHeader>
@@ -57,7 +60,7 @@ export const MemoryCard = async ({
 						<TooltipProvider>
 							<Tooltip>
 								<TooltipTrigger>
-									<Icons.lock className="h-4 w-4 text-muted-foreground" />
+									<Icons.lock className="text-muted-foreground h-4 w-4" />
 								</TooltipTrigger>
 								<TooltipContent>
 									<p>This memory is not public</p>
@@ -70,7 +73,7 @@ export const MemoryCard = async ({
 
 			<CardContent className="flex w-full flex-col items-center lg:w-3/4">
 				<div className="flex w-full flex-col items-center">
-					<span className="mb-2 self-center text-sm text-muted-foreground">
+					<span className="text-muted-foreground mb-2 self-center text-sm">
 						{dateFormat(new Date(created_at))}
 					</span>
 					{mediaType == 'image' ? (
@@ -94,13 +97,13 @@ export const MemoryCard = async ({
 						className="group ml-2 mt-2 flex items-center gap-1 self-start "
 					>
 						<Avatar className="h-5 w-5">
-							{user.avatar_url && <AvatarImage src={user.avatar_url} />}
+							{userAvatar && <AvatarImage src={userAvatar} />}
 							<AvatarFallback>
 								{user?.full_name?.at(0)?.toUpperCase()}
 							</AvatarFallback>
 						</Avatar>
 
-						<div className="flex items-center gap-1 font-semibold text-muted-foreground transition-colors group-hover:text-muted-foreground/80">
+						<div className="text-muted-foreground group-hover:text-muted-foreground/80 flex items-center gap-1 font-semibold transition-colors">
 							<span>{user?.username || user?.full_name}</span>
 						</div>
 					</Link>
