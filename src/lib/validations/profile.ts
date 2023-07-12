d7ad0f88da9182c43611f4c3349e26abe47cf384
whyleonardo <pleasefloyd73@gmail.com>
2023-07-12 17:11:26 -0300

    feat: add more fields in profile validations

--- src/lib/validations/profile.ts
+++ src/lib/validations/profile.ts
@@ -2,6 +2,8 @@ import { z } from 'zod'
 
 export const profileSchema = z.object({
 	username: z.string().min(2).max(15),
+	bio: z.string().max(120, 'Bio must contain at most 120 character(s)'),
+	avatar: z.any(),
 	fullname: z.string().min(2).max(18),
 	website: z.union([
 		z
