bb402bd33d7ff5d92b3140c6e16b845cd277516b
whyleonardo <pleasefloyd73@gmail.com>
2023-07-18 19:28:47 -0300

    feat: refactor: adjust types to sync with database table

--- src/types/profile.ts
+++ src/types/profile.ts
@@ -1,16 +1,3 @@
-export type Profile =
-	| {
-			avatar_url: string | null
-			email: string | null
-			full_name: string | null
-			id: string
-			updated_at: string | null
-			username: string | null
-			website: string | null | undefined
-			stripeCurrentPeriodEnd: Date
-			stripeCustomerId: string
-			stripePriceId: string
-			stripeSubscriptionId: string | null
-	  }
-	| undefined
-	| null
+import { Database } from './supabase'
+
+export type Profile = Database['public']['Tables']['profiles']['Row']
